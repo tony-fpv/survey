61a7c5ac5f1a89919ae4d4fd6b5cb38ff66f16b5
Tony <peppedda3@gmail.com>
2018-08-02 21:54:22 +0100

    Definitive version of survey.py

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# survey
+This is a CLI application to parse and display survey data from CSV files, and display the results in an output file.
+
+Prerequisites
+
+Install Python 
+Installl Pandas library
+Install Numpy library
+Install argparse library
+
+The exercise is probably it has been designed for Ruby and push data output in Tableau, but I preferred use 
+Python because is much performant (in my opinion) for any BI tool, included the possibility to be used in Machine Learning
+with iPython notebooks launched by Sagemaker.
+
+The next step it should be (my advice) is put everything in a AWS Datapipeline and populate any BI tools like Tableau
+R-Server or Kibana (ELK) .
+
+The comments are inside the file survey.py and the syntax to be launched is:
+
+python3 survey.py --inputfile survey-1.csv --outputfile output1.txt
+python3 survey.py --inputfile survey-2.csv --outputfile output2.txt
+python3 survey.py --inputfile survey-3.csv --outputfile output3.txt
+
+All the files could be used in a bash script and executed in interval wait of 10 seconds depending by the amount of data.
+
--- Tony_code/survey.py
+++ Tony_code/survey.py
@@ -1,12 +1,11 @@
 """
-
-put what this code does here
-
+A script to read, process and output a single file.
 """
 
 import pandas as pd
 import numpy as np
 import argparse
+
 
 def read_args():
     parser = argparse.ArgumentParser()
@@ -21,59 +20,76 @@ def read_args():
 
     return tuple(files)
 
-def get_results(surveys, survey_questions):
+
+def get_results(surveys):
+    """
+    Calculate the percentage of participation.
+    
+    :param surveys: data frame
+    :return: data frame
+    """
     survey_summary = {}
     submitted_surveys = surveys[surveys['SubmittedAt'].notnull()]
     participation_count = submitted_surveys.shape[0]
-    participation_percentage = round((submitted_surveys.shape[0]/surveys.shape[0]) * 100, 2)
+    participation_percentage = round((submitted_surveys.shape[0] / surveys.shape[0]) * 100, 2)
     survey_summary['participation_count'] = participation_count
     survey_summary['participation_percentage'] = participation_percentage
 
-    # you need to check if survey_questions dataframe are rating type questions before doing the following
-
-    for column in submitted_surveys.columns[3:]:
-        survey_summary[column+"_mean_rating"] = submitted_surveys[column].mean()
+    for column in submitted_surveys.columns:
+       print(column)
+       if(submitted_surveys[column].dtype == np.float64 or submitted_surveys[column].dtype == np.int64):
+          survey_summary[column + "_mean_rating"] = submitted_surveys[column].mean()
 
     return survey_summary
 
+
 def write_output(survey_summary, outputfile):
+    """
+    Write an output file.
+    
+    :param survey_summary: str
+    :param outputfile: str
+    :return: None
+    """
+
     f = open(outputfile, "w")
     for key, value in survey_summary.items():
         f.write(str(key) + " : " + str(value) + "\n")
     f.close()
 
+
 if __name__ == '__main__':
-    
     print("running....")
     (inputfile, outputfile) = read_args()
 
-    columns_survey1_responses = ['Email'
-      , 'EmployeeId'
-      , 'SubmittedAt'
-      , 'Answer1'
-      , 'Answer2'
-      , 'Answer3'
-      , 'Answer4'
-      , 'Answer5']
+    columns_survey1_responses = ['Email',
+                                'EmployeeId',
+                                'SubmittedAt',
+                                'Answer1',
+                                'Answer2',
+                                'Answer3',
+                                'Answer4',
+                                'Answer5']
 
-    columns_survey1 = ['theme'
-        , 'type'
-        , 'text']
-    
-    responses = {'survey-1.csv':'survey-1-responses.csv', 'survey-2.csv':'survey-2-responses.csv', 'survey-3.csv':'survey-3-responses.csv' }
+    columns_survey1 = ['theme',
+                      'type',
+                      'text']
 
-    # gets response file name based on survey file name
+    responses = {'survey-1.csv': 'survey-1-responses.csv', 
+                 'survey-2.csv': 'survey-2-responses.csv',
+                 'survey-3.csv': 'survey-3-responses.csv'}
+
+    # get response file name based on survey file name
     filename = responses.get(inputfile)
     print(filename)
 
-    # reads response file name as a pandas dataframe
-    survey1_responses_df = pd.read_csv(filename, header=None, sep=',', skip_blank_lines=True,names=columns_survey1_responses)
-    survey1_df = pd.read_csv(inputfile, header=None, sep=',', skip_blank_lines=True,names=columns_survey1)
+    # read response file name as a pandas dataframe
+    survey1_responses_df = pd.read_csv(filename, header=None, sep=',', skip_blank_lines=True,
+                                       names=columns_survey1_responses)
+    survey1_df = pd.read_csv(inputfile, header=None, sep=',', skip_blank_lines=True, names=columns_survey1)
 
-    # gets the required output results
-    survey_results = get_results(survey1_responses_df, survey1_df)
+    # get the required output results
+    survey_results = get_results(survey1_responses_df)
 
-    # writes the output to the output file
+    # write the output to the output file
     write_output(survey_results, outputfile)
-
-
